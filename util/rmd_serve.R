a2c9c6ae05bed5d57e91ae1b223b6279779d6399
Micah J Waldstein <micah@waldste.in>
2018-06-02 13:22:45 -0400

    Initial exploration

---
+++ util/rmd_serve.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+args <- commandArgs(trailingOnly = TRUE)
+
+directory <- if (length(args) > 0) {
+  args[1]
+} else {
+  "posts"
+}
+
+port <- if (length(args) > 1) {
+  as.integer(args[2])
+} else {
+  8080
+}
+
+if (!dir.exists(directory)) {
+  stop(paste0("Directory does not exist: ", directory))
+}
+
+library(servr)
+rmdv2(dir = directory, port = port)
